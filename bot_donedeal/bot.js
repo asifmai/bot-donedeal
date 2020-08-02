b14698e90fa6ab228148eecb6d57f9e1962cbec8
iqbal <asifmai@hotmail.com>
2020-08-02 14:07:03 +0500

    update

--- bot_donedeal/bot.js
+++ bot_donedeal/bot.js
@@ -79,8 +79,7 @@ const fetchCar = (carIdx) => new Promise(async (resolve, reject) => {
     car.doors = await getCellValue('doors', specs);
     car.price = await pupHelper.getTxt('.price-info__left-options span.price', page);
     car.price = car.price.replace(/,/gi, '').trim()
-    // car.description
-    // car.images
+    car.images = await pupHelper.getAttr('.gallery-media-content img', 'src', page);
     console.log(car);
 
     await page.close();
